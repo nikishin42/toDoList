4340c92ab213052095230fc5e5d3f66efebe9bbe
nikishin42 <cstarmie@si-c1.kzn.21-school.ru>
2022-03-31 19:03:21 +0300

    ошибки обрабатываются методами-помощниками

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,6 +12,12 @@ type neuteredFileSystem struct {
 	fs http.FileSystem
 }
 
+// структура для хранения всех зависимостей веб-приложения
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":8000", "Сетевой адрес HTTP")
 	flag.Parse()
@@ -19,10 +25,15 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
