71670bbbb89305f93cc170e76f79f49b7ca8acd7
Cathi Starmie <cstarmie@si-c1.kzn.21-school.ru>
2022-03-31 15:52:20 +0300

    add README and logs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
@@ -12,8 +13,11 @@ type neuteredFileSystem struct {
 }
 
 func main() {
-	addr := flag.String("addr", ":8220", "Сетевой адрес HTTP")
+	addr := flag.String("addr", ":8000", "Сетевой адрес HTTP")
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
@@ -21,12 +25,18 @@ func main() {
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
-	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Start serve %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	// Эта структура позволяет записывать ошибки, которые происходят на сервере в errorLog
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Start serve %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
 
 func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
